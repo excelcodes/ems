43ea18743f7930c6396c0168b43330c76284f035
Mohammad Almajarish <rayzerpix@gmail.com>
2023-11-11 13:59:02 +0400

    support google analytics

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,14 @@
+"use client"
+import React from "react";
 import "./globals.css";
 
+import ReactGA from "react-ga4";
+
 export default function RootLayout({ children }) {
+  React.useEffect(() => {
+    ReactGA.initialize(process.env.NEXT_PUBLIC_MEASUREMENT_ID);
+    ReactGA.send({ hitType: "pageview", page: "/", title: "EMS" });
+  }, []);
   return (
     <html lang="en">
       <head>
