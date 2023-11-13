7ccb78c93db67cf48c6c845cf6302b460553eb2c
Mohammad Almajarish <rayzerpix@gmail.com>
2023-11-13 16:40:10 +0400

    sw

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const withPWA = require('next-pwa')({
 })
 const nextConfig = {
   ...withPWA({
+    pwa: {
+      disable: process.env.NODE_ENV === "development"
+    },
     output: "export",
     distDir: "dist",
     images: {
