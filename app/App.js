16092d687128fd460bbf4ec60f4e32df5d70822e
Mohammad Almajarish <rayzerpix@gmail.com>
2023-11-19 21:44:09 +0400

    remove ad

--- app/App.js
+++ app/App.js
@@ -2,23 +2,16 @@
 import React from "react";
 import { usePathname } from "next/navigation";
 import ReactGA from "react-ga4";
-import Swal from "sweetalert2";
-import withReactContent from "sweetalert2-react-content";
 export default function App({ children }) {
-
   const pathname = usePathname()
 
   React.useEffect(() => {
-    withReactContent(Swal).fire({
-      title: "أحتاج مساعدة",
-      text: "لو تحب تساعد مطور الموقع عشان يستمر بتقديم الأفضل سويله كونسبت ماب.\nللتفاصيل تواصل عالرقم 0502563890"
-    })
     ReactGA.initialize(process.env.NEXT_PUBLIC_MEASUREMENT_ID);
   }, []);
 
   React.useEffect(() => {
     ReactGA.send({ hitType: "pageview", page: pathname, title: pathname });
-  }, [pathname])
+  },[pathname])
   return (
     <html lang="en">
       <head>
