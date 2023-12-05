9ee1401e316452f6a00da450fac72d1652197d20
Excel <rayzerpix@gmail.com>
2023-12-05 20:34:39 +0400

    shut down

--- app/App.js
+++ app/App.js
@@ -3,21 +3,22 @@ import React from "react";
 import { usePathname } from "next/navigation";
 import ReactGA from "react-ga4";
 export default function App({ children }) {
-  const pathname = usePathname()
+//   const pathname = usePathname()
 
-  React.useEffect(() => {
-    ReactGA.initialize(process.env.NEXT_PUBLIC_MEASUREMENT_ID);
-  }, []);
+//   React.useEffect(() => {
+//     ReactGA.initialize(process.env.NEXT_PUBLIC_MEASUREMENT_ID);
+//   }, []);
 
-  React.useEffect(() => {
-    ReactGA.send({ hitType: "pageview", page: pathname, title: pathname });
-  },[pathname])
+//   React.useEffect(() => {
+//     ReactGA.send({ hitType: "pageview", page: pathname, title: pathname });
+//   },[pathname])
   return (
     <html lang="en">
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=.6" />
-      </head>
-      <body>{children}</body>
+      // <head>
+      //   <meta name="viewport" content="width=device-width, initial-scale=.6" />
+      // </head>
+      // <body>{children}</body>
+    <body>see you next semester &#60;3</body>
     </html>
   )
 }
